bcb655a245a48abd9a2667a9910f745debbf0e32
Hunter Padilla <Hpadilla2020@FAU.edu>
2024-01-08 12:36:02 -0500

    Initial Commit

---
+++ AlzDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlzDetection
+//
+//  Created by Hunter Padilla on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AlzDetectionUITests/AlzDetectionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AlzDetectionUITestsLaunchTests.swift
+//  AlzDetectionUITests
+//
+//  Created by Hunter Padilla on 1/8/24.
+//
+
+import XCTest
+
+final class AlzDetectionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
