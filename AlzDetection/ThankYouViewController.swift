d1f7375e8829f74f863577c377c39b745129f459
Schen230 <sunnychen230@gmail.com>
2024-01-13 10:55:04 -0500

    Added Thank You Page Scene and adjusted layout to match the App Subsystem Diagram. Made ThankYouViewController.

---
+++ AlzDetection/ThankYouViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ThankYouViewController.swift
+//  AlzDetection
+//
+//  Created by Sunny Chen on 1/13/24.
+//
+
+import UIKit
+
+class ThankYouViewController: UIViewController {
+
+    
+    @IBAction func MainMenuButton(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
