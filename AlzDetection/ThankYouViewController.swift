7814a08256a7a007e44b887918dbe86211507fc0
Schen230 <sunnychen230@gmail.com>
2024-01-13 12:24:06 -0500

    Test

--- AlzDetection/ThankYouViewController.swift
+++ AlzDetection/ThankYouViewController.swift
@@ -19,6 +19,9 @@ class ThankYouViewController: UIViewController {
     
     
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
