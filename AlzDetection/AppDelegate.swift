116ad981c978e3a66cd1c6616f417de153fce795
Hunter Padilla <Hpadilla2020@FAU.edu>
2024-01-18 23:14:24 -0500

    Implement Login/Logout and Sign Up Functionality
    User info is stored in AWS Amplify

--- AlzDetection/AppDelegate.swift
+++ AlzDetection/AppDelegate.swift
@@ -4,12 +4,15 @@
 //
 //  Created by Hunter Padilla on 1/8/24.
 //
-
+import Amplify
+import AWSAPIPlugin
+import AWSCognitoAuthPlugin
+import AWSDataStorePlugin
+import SwiftUI
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -31,6 +34,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    //Initializes AWS Auth on app Launch (Check builds for confirmation)
+    override init() {
+            do {
+                try Amplify.add(plugin: AWSCognitoAuthPlugin())
+                try Amplify.configure()
+                print("Amplify configured with auth plugin")
+            } catch {
+                print("Failed to initialize Amplify with \(error)")
+            }
+        }
+    
+    //Checks to see who is currently logged into Auth Session
+    func fetchCurrentAuthSession() async {
+        do {
+            let session = try await Amplify.Auth.fetchAuthSession()
+            print("Is user signed in - \(session.isSignedIn)")
+        } catch let error as AuthError {
+            print("Fetch session failed with error \(error)")
+        } catch {
+            print("Unexpected error: \(error)")
+        }
+    }
+    
+    
+    
 }
 
