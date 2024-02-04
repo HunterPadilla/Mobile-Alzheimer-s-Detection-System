32376dc480e19850b1ba1e8114f7ef52771697e5
Schen230 <sunnychen230@gmail.com>
2024-02-03 21:11:19 -0500

    Results table update

---
+++ AlzDetection/TestCell.swift
@@ -0,0 +1,18 @@
+//
+//  TestCell.swift
+//  AlzDetection
+//
+//  Created by Sunny Chen on 2/3/24.
+//
+
+import Foundation
+import UIKit
+
+class TestCell : UITableViewCell {
+    
+    @IBOutlet weak var trialNum: UILabel!
+    
+    override func layoutSubviews() {
+        
+    }
+}
