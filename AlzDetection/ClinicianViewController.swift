357c7f5e63f71b08c3990ee359b21e64a0653141
Schen230 <sunnychen230@gmail.com>
2024-03-16 14:16:58 -0400

    Clinician Notification
    ClinicianViewController has been updated to use MFMailCompose to send an email notifying clinician of results.

--- AlzDetection/ClinicianViewController.swift
+++ AlzDetection/ClinicianViewController.swift
@@ -19,11 +19,11 @@ class ClinicianViewController: UIViewController, MFMailComposeViewControllerDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //These two lines are needed to disable the keyboard on Mobile phone for testing
-        //Note that UITextFieldDelegate is also added to the class for this functionality
-        self.ClinicianEmail.delegate = self
-        self.ClinicianPhone.delegate = self
+//
+//        //These two lines are needed to disable the keyboard on Mobile phone for testing
+//        //Note that UITextFieldDelegate is also added to the class for this functionality
+//        self.ClinicianEmail.delegate = self
+//        self.ClinicianPhone.delegate = self
     }
     
     //The following functions disbale the keyboard when using a mobile phone for testing
@@ -36,8 +36,6 @@ class ClinicianViewController: UIViewController, MFMailComposeViewControllerDele
         self.view.endEditing(true)
     }
     
-    
-
     @IBAction func ViewResultsButton(_ sender: Any) {
         guard let ResultsPage = storyboard? .instantiateViewController(withIdentifier: "Results") as? ResultsViewController else {
             return
@@ -45,36 +43,67 @@ class ClinicianViewController: UIViewController, MFMailComposeViewControllerDele
         present(ResultsPage, animated: true)
     }
     
-//    @IBAction func SendResults(_sender: Any) {
-//        Task { @MainActor in
-//            
-//            let email: String = ClinicianEmail.text!
-//            let phone: String = ClinicianPhone.text!
-//            
-//            if MFMailComposeViewController.canSendMail(){
-//                let composeMail = MFMailComposeViewController()
-//                composeMail.delegate = self
-//                composeMail.setSubject("Patient Results Available")
-//                composeMail.setToRecipients([email])
-//                composeMail.setMessageBody(<#T##body: String##String#>, isHTML: true)
-//                composeMail.addAttachmentData(<#T##attachment: Data##Data#>, mimeType: <#T##String#>, fileName: <#T##String#>)
-//                present(composeMail, animated: true)
-//                present(UINavigationController(rootViewController: composeMail), animated: true)
-//                guard let ResultsSent = storyboard? .instantiateViewController(withIdentifier: "SentResults") as? ResultsSentViewController else {
-//                    return
-//                }
-//                present(ResultsSent, animated: true)
-//            }
-//            
-//            else {
-//                print("Email failed to send")
-//                return
-//            }
-//            
-//        }
-        
-        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-            controller.dismiss(animated: true, completion: nil)
+    @IBAction func SendResults(_sender: Any) {
+        guard self.isViewLoaded && (self.view.window != nil) else {
+            print("View is not in the window hierarchy.")
+            return
         }
         
+        Task { @MainActor in
+            
+            //let email: String = ClinicianEmail.text!
+            //let phone: String = ClinicianPhone.text!
+            let email: String = "schen2014@fau.edu"
+            
+            let htmlContent = "<html><body><p>Your patient Sunny is sending you results from a new trial!</p></body></html>"
+            
+            //let fileData: Data? = await fetchFileData()
+            //let mimeType = "application/mp4"
+            //let fileName = "Results.mp4"
+            
+            if MFMailComposeViewController.canSendMail(){
+                let composeMail = MFMailComposeViewController()
+                composeMail.mailComposeDelegate = self
+                composeMail.setSubject("Patient Results Available")
+                composeMail.setToRecipients([email])
+                composeMail.setMessageBody(htmlContent, isHTML: true)
+                
+                //if let fileData = fileData {
+                //    composeMail.addAttachmentData(fileData, mimeType: mimeType, fileName: fileName)
+                //}
+                DispatchQueue.main.async{
+                    self.present(composeMail, animated: true)
+                }
+            }
+            else {
+                let alert = UIAlertController(title: "Mail Not Available", message: "Mail services are not available. Please configure an email account in the Mail app to send emails.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
+        
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true) {
+            
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: "There was an error sending your email: \(String(describing: error?.localizedDescription))", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            if result == .sent {
+                guard let resultsSentVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultsSent") as? ResultsSentViewController
+                else {
+                    return
+                }
+                self.present(resultsSentVC, animated: true)
+            }
+            else if result == .cancelled {
+                let cancelAlert = UIAlertController(title: "Email cancelled.", message: "Your clinician has not been notified about your results.", preferredStyle: .alert)
+                self.present(cancelAlert, animated: true, completion: nil)
+            }
+        }
+    }
+}
--- AlzDetection/ResultsViewController.swift
+++ AlzDetection/ResultsViewController.swift
@@ -19,13 +19,13 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
         present(MainMenu, animated: true)
     }
     
+    
+    
     struct Results {
         let title: String
         let imageName: String
     }
-    
     var listResult : [StorageListResult?] = []
-
     let trialList : [Results] = [
         Results(title: "Trial 1", imageName: "AppIcon"),
         Results(title: "Trial 2", imageName: "AppIcon"),
@@ -33,13 +33,14 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
         Results(title: "Trial 4", imageName: "AppIcon"),
         Results(title: "Trial 5", imageName: "AppIcon")
     ]
+    var testList = [String]()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         Table.dataSource = self
         Table.delegate = self
-        
-        
         Task{
             //let userID = "\(try await Amplify.Auth.getCurrentUser().userId)"
             //let options = StorageListRequest.Options(accessLevel: .guest ,targetIdentityId: "userID", pageSize: 1000)
@@ -47,14 +48,14 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
             let listResult = try await Amplify.Storage.list(options: options)
             listResult.items.forEach { item in
                 print("Key:\(item.key)")
+                testList.append(item.key)
+                
             }
+            testList.removeFirst(1)
+            dump(testList)
         }
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return trialList.count
     }
@@ -62,10 +63,10 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let Results = trialList[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath) as! TableViewCell
+
         cell.label.text = Results.title
-        
         cell.iconImageView.image = UIImage(named: Results.imageName)
-        cell.iconImageView.layer.cornerRadius = cell.iconImageView.frame.height / 3
+        cell.iconImageView.layer.cornerRadius = cell.iconImageView.frame.height / 6
         cell.iconImageView.layer.borderWidth = 1
         cell.iconImageView.layer.masksToBounds = false
         cell.iconImageView.layer.borderColor = UIColor.white.cgColor
